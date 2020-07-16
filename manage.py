a0dfc6d955cf194b3cfab9f5900677fc4fae176e
kangalah <nyongesajoannanjala@gmail.com>
2020-07-16 10:47:06 +0300

    import manager from flask-script

--- manage.py
+++ manage.py
@@ -1,5 +1,14 @@
+from flask_script import Manager,Server
+from app import create_app,db
+from app.models import User
+# Creating app instance
+app = create_app('development')
 
-from app import app
+manager = Manager(app)
+manager.add_command('server',Server)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 if __name__ == '__main__':
-    app.run(debug = True)
+    manager.run()
