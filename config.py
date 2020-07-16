f39e378e6351f530cea05b2bdf077e5ab935ab1a
Munge Ryan <mungeryan@gmail.com>
2020-07-16 12:25:57 +0300

    configuring development

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pizza'
     pass
 
 
@@ -9,7 +11,6 @@ class Config:
 class ProdConfig(Config):
     '''
     Production  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
@@ -19,9 +20,14 @@ class ProdConfig(Config):
 class DevConfig(Config):
     '''
     Development  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+# 'test':TestConfig
+}
